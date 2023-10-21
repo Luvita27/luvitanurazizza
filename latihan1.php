0d3e24b54d02ececcb70bf96177f0392f39d083d
Luvita27 <81553088+Luvita27@users.noreply.github.com>
2023-10-21 16:02:54 +1300

    Add files via upload

---
+++ latihan1.php
@@ -0,0 +1,85 @@
+<!doctype html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang=""> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9" lang=""> <![endif]-->
+<!--[if gt IE 8]><!-->
+<html class="no-js" lang="">
+<!--<![endif]-->
+<head>
+<meta charset="utf-8">
+<meta name="description" content="">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Luvita</title>
+<link rel="stylesheet" href="css/bootstrap.min.css">
+<link rel="stylesheet" href="css/jquery.fancybox.css">
+<link rel="stylesheet" href="css/main.css">
+<link rel="stylesheet" href="css/responsive.css">
+<link rel="stylesheet" href="css/animate.min.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+</head>
+
+<body class="bg-info">
+
+ <!-- Move to up button -->
+ <div class="scroll-button">
+      <a href="#home"><i class=""></i></a>
+    </div>
+    <!-- navgaition menu -->
+    <header id="header">
+    <div class="header-content clearfix bg-info"> <a class="logo" href="#">Luvita</a>
+      <nav class="navigation" role="navigation">
+        <ul class="primary-nav">
+          <li><a href="#introduction">About Me</a></li>
+          <li><a href="#contact">Contact Me</a></li>
+        </ul>
+      </nav>
+      <a href="#" class="nav-toggle">Menu<span></span></a> </div>
+  </header>
+  <br />
+  <br /><br /><br /><br /><br />
+
+
+<div class="container mt-5">
+    
+    <div class="row d-flex justify-content-center">
+        
+        <div class="col-md-12">
+            
+            <div class="card">
+                
+                <div class="text-center">
+                    <img src="images.png" width="100" class="rounded-circle">
+                </div>
+                
+                <div class="text-center mt-3">
+                    <h5 class="mt-2 mb-0">Luvita Nur Azizza Ningrum</h5>
+                    <span>Sistem Informasi</span>
+                    <hr >
+                    <div class="px-4 mt-1">
+                        <p class="text-dark"> Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+ sed do eiusmod tempor incididunt ut labore et dolore magna 
+aliqua.
+ Ut enim ad minim veniam, quis nostrud exercitation ullamco 
+laboris
+ nisi ut aliquip ex ea commodo consequat. </p>
+                    
+                    </div>   
+                </div>
+                 </div>
+            
+        </div>
+        
+    </div>
+    
+</div> 
+
+
+<!-- JS FILES --> 
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script> 
+<script src="js/bootstrap.min.js"></script> 
+<script src="js/jquery.fancybox.pack.js"></script> 
+<script src="js/retina.min.js"></script> 
+<script src="js/modernizr.js"></script> 
+<script src="js/main.js"></script>
+</body>
+</html>
